a781a79425631e09de2d06c2a64e58c5dfda4220
flaviaahvs <161638358+flaviaahvs@users.noreply.github.com>
2025-06-25 11:13:40 -0300

    25

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -30,7 +31,15 @@ function calculaTempo(tempoObjetivo) {
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
-    
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0) {
+        return [dias, horas, minutos, segundos];
+    } else {
+        return [0, 0, 0, 0];
+    }
+}
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
